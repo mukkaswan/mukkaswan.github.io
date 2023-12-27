e9b05c2c1f97ff78f1276ff38dfac0c7c066af3a
mukkaswan <63834041+mukkaswan@users.noreply.github.com>
2023-12-27 20:22:22 +0530

    Add files via upload

---
+++ classroom_script.js
@@ -0,0 +1,12 @@
+/* Script to Toggle Topbar */
+
+function togglewn() {
+  var x = document.getElementById("win1");
+  if (x.style.display === "none") {
+    x.style.display = "block";
+  } else {
+    x.style.display = "none";
+  }
+}
+
+
